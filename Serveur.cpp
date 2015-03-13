e4e7588dae7ee526230d0873db43042640a9b59b
Loïc Touzard <loic.touzard@gmail.com>
2015-03-13 19:57:05 +0100

    Modif Alexis

--- Serveur.cpp
+++ Serveur.cpp
@@ -1,39 +1,29 @@
 #ifndef SERVEUR_H
 #define	SERVEUR_H
-
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-
 class Serveur {
 public:
-    int taille;
-    int capacite;
-    
-    int rangee;
-    int emplacement;
-
-    float rendement;
-    
-    bool positionne;
-    
-    int groupe;
-    
-    Serveur(int t, int cap) : taille(t), capacite(cap)
-    {
-        rangee = -1;
-        emplacement = -1;
-        groupe = -1;
-        
-        positionne = false;
-        
-        rendement = (float)capacite / (float)taille;
-        //cout << rendement << " = " << capacite << " / " << taille << endl;;
-    }
-    
-    virtual ~Serveur(){}
+int taille;
+int capacite;
+int rangee;
+int emplacement;
+float rendement;
+bool positionne;
+int groupe;
+int indice;
+Serveur(int t, int cap, int i) : taille(t), capacite(cap)
+{
+indice = i;
+rangee = -1;
+emplacement = -1;
+groupe = -1;
+positionne = false;
+rendement = (float)capacite / (float)taille;
+//cout << rendement << " = " << capacite << " / " << taille << endl;;
+}
+virtual ~Serveur(){}
 private:
-
 };
-
 #endif	/* SERVEUR_H */
--- main.cpp
+++ main.cpp
@@ -3,175 +3,188 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
-
-
 #include "Serveur.cpp"
 #include "Emplacement.cpp"
 #include "Groupe.cpp"
 #define MAP
 using namespace std;
-
 vector<Serveur*> servOrdreInit, servOrdreRend;
 vector<vector<Emplacement> > tab;
-
 vector<Groupe> listGroupes;
-
+vector<int> capacite;
 int R, S, U, P, M;
 
+void afficheBaies(vector<vector<Emplacement> > & baies)
+{
+cout << "affiche" <<endl;
+cout << baies.size() << endl;
+	for(int i =0; i < baies.size(); i ++)
+	{
+	cout << "ligne " << i <<": ";
+		for(int j = 0; j < baies[i].size(); j ++)
+		{
+			if(baies[i][j].serv != NULL)
+			{
+				cout << baies[i][j].serv->indice <<" ";
+			}
+			else if(baies[i][j].etat == Emplacement::LIBRE)
+			{
+				cout << "v ";
+			}
+			else
+			{
+				cout <<"x ";
+			}
+		}
+		cout << endl;
+	}
+
+
+}
+
+void afficherGroupe()
+{
+	for(int i = 0; i < P; i ++)
+	{
+	
+	
+	}
+
+
+}
 // FN SORT des serveurs par rendement
 bool sortByRend(Serveur* s1, Serveur* s2)
 {
-	//return true si il faut S1 passe avant S2
-	if(s1->rendement > s2->rendement)
-	{
-		return true;
-	}
-	else if (s1->rendement == s2->rendement)
-	// si rendement egal on prend le plus petit
-	{		
-		if(s1->taille < s2->taille)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	else
-	{
-		return false;
-	}
+//return true si il faut S1 passe avant S2
+if(s1->rendement > s2->rendement)
+{
+return true;
 }
-
+else if (s1->rendement == s2->rendement)
+// si rendement egal on prend le plus petit
+{
+if(s1->taille < s2->taille)
+{
+return true;
+}
+else
+{
+return false;
+}
+}
+else
+{
+return false;
+}
+}
 // INIT tous les var globaux => lecture du fichier
 void init(const string& s)
 {
-    ifstream file;
-    file.open(s);
-    
-    string line;
-    
-    if (!file.good())
-    {
-        cout << "ERREUR LECTURE" << endl;
-        return;
-    }
-    
-    // Premiere ligne
-    file >> line;
-    R = stoi(line);
-    file >> line;
-    S = stoi(line);
-    file >> line;
-    U = stoi(line);
-    file >> line;
-    P = stoi(line);
-    file >> line;
-    M = stoi(line);
-    
-    // Création tableaux
-    for (int i = 0; i < R; ++i)
-    {
-        vector<Emplacement> v;
-        tab.push_back(v);
-        for (int j = 0; j<S; ++j)
-        {
-            Emplacement e;
-            tab[i].push_back(e);
-        }
-    }
-    
-    
-    // Emplacements indispo
-    string line2;
-    int r, e;
-    for (int i = 0; i < U; ++i)
-    {
-        file >> line;
-        file >> line2;
-        r = stoi(line);
-        e = stoi(line2);
-        tab[r][e].etat = Emplacement::INDISPO;
-    }
-    
-    // Lecture des serveurs (remplissage des tableaux par ordre d'ajout)
-    for(int i = 0; i < M; ++i)
-    {
-       file >> line;
-       file >> line2;
-       servOrdreInit.push_back(new Serveur(stoi(line), stoi(line2)));
-    }
-    
-    file.close();
-    
-    // Remplissage des tableaux triés (copie & tri)
-    servOrdreRend.resize(M);
-    copy(servOrdreInit.begin(), servOrdreInit.end(), servOrdreRend.begin());
-    
-    sort(servOrdreRend.begin(), servOrdreRend.end(), sortByRend);
-    
-    for (int i = 0; i < P; ++i)
-    {
-        Groupe gr(R);
-        listGroupes.push_back(gr);
-    }
-}
-
-int verifRangee(vector<Emplacement>& rangee, int taille_s);
-int rechercheCase(vector<Emplacement> barre, Serveur* serv);
-
-bool rechercheEmplacement(vector<vector<Emplacement>> & barres, Serveur* servIns)
+ifstream file;
+file.open(s);
+int line;
+if (!file.good())
 {
-	vector<Emplacement> b_min = barres[0];
-	int taille_s = servIns->taille;
-	int val_min = 1000;//verifRangee(b_min, taille_s);
-	int val_tmp = 0;
-        cout << "Val tmp " << val_tmp << endl;
-	for(int it_col = 0; it_col < barres.size(); it_col++)
+cout << "ERREUR LECTURE" << endl;
+return;
+}
+// Premiere ligne
+file >> R;
+//R = stoi(line);
+file >> S;
+//S = stoi(line);
+file >> U;
+//U = stoi(line);
+file >> P;
+//P = stoi(line);
+file >> M;
+//M = stoi(line);
+// Cr�ation tableaux
+for (int i = 0; i < R; ++i)
+{
+vector<Emplacement> v;
+tab.push_back(v);
+for (int j = 0; j<S; ++j)
+{
+Emplacement e;
+tab[i].push_back(e);
+
+}
+}
+// Emplacements indispo
+int line2;
+int r, e;
+for (int i = 0; i < U; ++i)
+{
+file >> r;
+file >> e;
+//r = stoi(line);
+//e = stoi(line2);
+tab[r][e].etat = Emplacement::INDISPO;
+}
+// Lecture des serveurs (remplissage des tableaux par ordre d'ajout)
+for(int i = 0; i < M; ++i)
+{
+file >> line;
+file >> line2;
+servOrdreInit.push_back(new Serveur(line,line2,i));
+}
+file.close();
+// Remplissage des tableaux tri�s (copie & tri)
+servOrdreRend.resize(M);
+copy(servOrdreInit.begin(), servOrdreInit.end(), servOrdreRend.begin());
+sort(servOrdreRend.begin(), servOrdreRend.end(), sortByRend);
+for (int i = 0; i < P; ++i)
+{
+Groupe gr(R);
+listGroupes.push_back(gr);
+}
+}
+bool verifRangee(vector<Emplacement>& rangee, int taille_s);
+int rechercheCase(vector<Emplacement>& barre, Serveur* serv);
+int insererServ( vector<Emplacement>& barre,int emplacement, Serveur* serv)
+{
+	
+	for(int i = 0; i < serv->taille; i++)
 	{
-		val_tmp = verifRangee(barres[it_col], taille_s);
-                cout << "Val tmp " << val_tmp << endl;
-                cout << "Val min " << val_min << endl;
-		if(val_tmp < val_min && val_tmp >= 0) //si barre ok et plus petite capacite
-		{
-                    cout << "RENTRE" << endl;
-			val_min = val_tmp;
-			b_min = barres[it_col];
-                        
-                        servIns->rangee = it_col;
-                        
-		}
+		barre[emplacement+i].etat = Emplacement::PRIS;
+		barre[emplacement+i].serv = serv;
+		
 	}
-            #ifdef MAP
-                cout << "insertion en rangee " << servIns->rangee << endl;
-                #endif
-	if(val_min < 0)
+	serv->emplacement = emplacement;
+
+}
+int rechercheEmplacement(vector<vector<Emplacement>> & barres, Serveur* servIns)
+{
+vector<Emplacement> b_min = barres[0];
+int taille_s = servIns->taille;
+int val_min = *min_element(capacite.begin(),capacite.end());
+bool trouve = false;
+int inser;
+for(int i = 0; i < barres.size(); i++)
+{
+cout <<"rangee " << i <<endl;
+	if(capacite[i] == val_min && verifRangee(barres[i],taille_s))
 	{
-		#ifdef MAP
-			cout << "pas d'emplacement dispo" << endl;
-		#endif
-		return false;
+		inser = i;
+		trouve = true;
+		break;
 	}
-	int indice = rechercheCase(b_min, servIns);
-	#ifdef MAP
-		cout << "insertion en empl " << indice << endl;
-	#endif
-	b_min[indice].serv = servIns;
-        servIns->emplacement = indice;
-        
-	for(int i = 0; i < servIns->taille; i++)
-	{
-		b_min[indice+i].etat = Emplacement::PRIS;
-                b_min[indice+i].serv = servIns;
-	}
-	return true;
 }
 
-
-
-int rechercheCase(vector<Emplacement> barre, Serveur* serv)
+if(trouve == true )
+{
+	int emplacement= rechercheCase(barres[inser],servIns);
+	cout <<"emplacement " << emplacement <<endl;
+	insererServ(barres[inser],emplacement,servIns);
+	servIns->rangee = inser;
+	capacite[inser] += servIns->capacite;
+	
+}
+cout <<endl;
+return 0;
+}
+int rechercheCase(vector<Emplacement>& barre, Serveur* serv)
 {
 int taille_tmp = 0;
 //int t_ret = 0;
@@ -179,186 +192,127 @@ int t_min=-1;
 int ind_min=-1;
 int i;
 for(i = 0; i < barre.size(); i++)
+{
+	if(barre[i].etat == Emplacement::LIBRE) //trou
 	{
-		if(barre[i].etat == Emplacement::LIBRE) //trou
+		taille_tmp++;
+		if (taille_tmp >= serv->taille)
 		{
-			taille_tmp++;
-                        if (taille_tmp == serv->taille)
-                        {
-                            return i - serv->taille + 1;
-		
-                        }        
-                }
-		else
-		{
-                    taille_tmp = 0;
-                    /*
-                    if(t_min < 0 && taille_tmp >= serv->taille)
-                    {
-                            t_min = serv->taille;
-                            ind_min = i - serv->taille;
-                    }
-                    else
-                    {
-                        if(taille_tmp >= serv->taille && taille_tmp < t_min)
-                        {
-                                t_min = serv->taille;
-                                ind_min = i - serv->taille;	
-                        }
-                    }*/
+			return i - serv->taille + 1;
 		}
-	}
-
-	//si la dernière case est disponible on doit mettre à jour les valeurs quand même
-	/*if(t_min < 0 && taille_tmp >= serv->taille)
-	{
-		t_min = serv->taille;
-		ind_min = i - serv->taille;
 	}
 	else
 	{
-		if(taille_tmp >= serv->taille && taille_tmp < t_min)
-		{
-			t_min = serv->taille;
-			ind_min = i - serv->taille;	
-		}
+		taille_tmp = 0;
 	}
-*/
-	return ind_min;
-
 }
 
-
-//renvoie la capacité ou -1 si insertion pas possible
-int verifRangee(vector<Emplacement>& rangee, int taille_s)
+return ind_min;
+}
+//renvoie la capacit� ou -1 si insertion pas possible
+bool verifRangee(vector<Emplacement>& rangee, int taille_s)
 {
-	int capacite=0;
-	int taille_max=0;
-	int taille_tmp =0;
-
-	//calcul de taille maximum ou on peut inserer
-	for(int i = 0; i < rangee.size(); i++)
-	{
-		if(rangee[i].etat == Emplacement::LIBRE) //trou
-		{
-			taille_tmp ++;
-		}
-		else
-		{
-			if(taille_max < taille_tmp)
-			{
-				taille_max = taille_tmp;
-				taille_tmp = 0;	
-			}
-		}
-	}
+int taille_max=0;
+int taille_tmp =0;
+//calcul de taille maximum ou on peut inserer
+for(int i = 0; i < rangee.size(); i++)
+{
+if(rangee[i].etat == Emplacement::LIBRE) //trou
+{
+	taille_tmp ++;
+}
+else
+{
 	if(taille_max < taille_tmp)
 	{
 		taille_max = taille_tmp;
-		taille_tmp = 0;	
 	}
-
-	if(taille_max < taille_s)
-	{
-		return -1; //pas bon
-	}
-
-	//Capacité
-	for(int i = 0; i < rangee.size();)
-	{
-		if(rangee[i].serv != NULL) //serveur dispo
-		{
-			capacite += rangee[i].serv->capacite;
-			i+=rangee[i].serv->taille;
-		}
-		else
-		{
-			i++;
-		}
-	}
-
-	return capacite;
-
-
+	taille_tmp = 0;
 }
-
-
+}
+if(taille_max < taille_tmp)
+{
+	taille_max = taille_tmp;
+}
+if(taille_max < taille_s)
+{
+	cout << "insertion pas ok" <<endl;
+	return false; //pas bon
+}
+else
+{
+cout <<"insertion ok" <<endl;
+return true;
+}
+}
 // Renvoie le num du groupe avec le min sur cette ligne
 int groupeMinLigne(int ligne)
 {
-	int groupeMin = 0;
-	int minActuel = listGroupes[0].capacitesSurLigne[ligne];
-
-	for (int i = 1; i < P; ++i)//sur tout les groupes
-	{
-		int min = listGroupes[i].capacitesSurLigne[ligne];
-		if (min < minActuel)
-		{
-			//on met le nouveau min
-			minActuel = min;
-			groupeMin = i;
-		}
-	}
-	return groupeMin;
+int groupeMin = 0;
+int minActuel = listGroupes[0].capacitesSurLigne[ligne];
+for (int i = 1; i < P; ++i)//sur tout les groupes
+{
+int min = listGroupes[i].capacitesSurLigne[ligne];
+if (min < minActuel)
+{
+//on met le nouveau min
+minActuel = min;
+groupeMin = i;
 }
-
-
+}
+return groupeMin;
+}
 void repartitionGroupe()
 {
-	//pour chaque serv
-	//for (vector<Serveur*>::iterator i =servOrdreInit != servOrdreInit.end(); ++i)
-            for (int i = 0; i < servOrdreInit.size(); ++i)
-        {
-		if (servOrdreInit[i]->rangee != -1)
-		{
-			// Quelle est le groupe avec le min sur cette ligne ?
-			int legroupe = groupeMinLigne(servOrdreInit[i]->rangee);
-			// Ajoute à celui-ci
-			servOrdreInit[i]->groupe = legroupe;
-			listGroupes[legroupe].capacitesSurLigne[servOrdreInit[i]->rangee] += servOrdreInit[i]->capacite;
-		}	
+//pour chaque serv
+//for (vector<Serveur*>::iterator i =servOrdreInit != servOrdreInit.end(); ++i)
+for (int i = 0; i < servOrdreInit.size(); ++i)
+{
+	if (servOrdreInit[i]->rangee != -1)
+	{
+		// Quelle est le groupe avec le min sur cette ligne ?
+		int legroupe = groupeMinLigne(servOrdreInit[i]->rangee);
+		// Ajoute � celui-ci
+		servOrdreInit[i]->groupe = legroupe;
+		listGroupes[legroupe].capacitesSurLigne[servOrdreInit[i]->rangee] += servOrdreInit[i]->capacite;
 	}
 }
-
+}
 void printOutFile()
 {
-	ofstream file;
-	file.open("result.out.txt", ios::out | ios::trunc);
-	for(unsigned int i = 0; i < servOrdreInit.size(); ++i)
-	{
-		if (servOrdreInit[i]->groupe == -1)
-		{
-			file << "x" << endl;
-		}
-		else
-		{
-			file << servOrdreInit[i]->rangee << " " << servOrdreInit[i]->emplacement << " " << servOrdreInit[i]->groupe << endl;
-		}
-	}
-	file.close();
+ofstream file;
+file.open("result.out.txt", ios::out | ios::trunc);
+for(unsigned int i = 0; i < servOrdreInit.size(); ++i)
+{
+if (servOrdreInit[i]->groupe == -1)
+{
+file << "x" << endl;
 }
-
-
-
-
-
-
-
+else
+{
+file << servOrdreInit[i]->rangee << " " << servOrdreInit[i]->emplacement << " " << servOrdreInit[i]->groupe << endl;
+}
+}
+file.close();
+}
 int main(int argc, char** argv)
 {
-    init(argv[1]);
-    
-    for (unsigned int i = 0; i < servOrdreRend.size(); ++i)
-    {
-        //cout << servOrdreRend[i]->rendement <<"\t" <<servOrdreRend[i]->taille <<  endl;
-        rechercheEmplacement(tab, servOrdreRend[i]);
-    }
-    
-    
-    repartitionGroupe();
-    printOutFile();
-    
-    
-    return 0;
-}
+init(argv[1]);
+capacite = vector<int> (tab.size(),0);
+for(int i = 0; i < capacite.size(); i++)
+cout << capacite[i] << endl;
+afficheBaies(tab);
+for (unsigned int i = 0; i < servOrdreRend.size(); ++i)
+{
+//cout << servOrdreRend[i]->rendement <<"\t" <<servOrdreRend[i]->taille << endl;
+cout << "serveur " <<servOrdreRend[i]->indice << "de taille" << servOrdreRend[i]->taille <<endl;
+rechercheEmplacement(tab, servOrdreRend[i]);
 
+afficheBaies(tab);
+
+cout << endl;
+}
+repartitionGroupe();
+printOutFile();
+return 0;
+}
